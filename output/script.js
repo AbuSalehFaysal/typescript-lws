47896581099b11e6172adf4330662d406fb06b55
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-06-13 11:44:05 +0600

    type basics

--- output/script.js
+++ output/script.js
@@ -1,3 +1,18 @@
 "use strict";
-const country = "Finland";
-console.log(country);
+// function
+const multiply = (a, b) => {
+    return a * b;
+};
+console.log(multiply(4, 3));
+// array
+let fruits = ["apple", "banana", "cherry"];
+fruits.push("orange");
+console.log(fruits);
+// object
+let person = {
+    name: "John",
+    age: 30,
+    isCaptain: true
+};
+person.age = 40;
+console.log(person);
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,24 @@
-const country = "Finland";
-console.log(country);
+// function
+const multiply = (a : number, b : number) => {
+    return a * b;
+}
+console.log(multiply(4, 3));
+
+// array
+let fruits = ["apple", "banana", "cherry"];
+fruits.push("orange");
+console.log(fruits);
+
+// object
+let person = {
+    name: "John",
+    age: 30,
+    isCaptain: true
+};
+person.age = 40;
+console.log(person);
+
+
+
+
 
