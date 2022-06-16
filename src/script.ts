6e39793f9ad6f0634dbcd85c7f72bac96ef0cb96
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-06-16 11:14:32 +0600

    explicit and union types

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,9 @@
+// START - LESSON - 03
+
 // function
-const multiply = (a : number, b : number) => {
-    return a * b;
-}
+const multiply = (a: number, b: number) => {
+  return a * b;
+};
 console.log(multiply(4, 3));
 
 // array
@@ -11,14 +13,36 @@ console.log(fruits);
 
 // object
 let person = {
-    name: "John",
-    age: 30,
-    isCaptain: true
+  name: "John",
+  age: 30,
+  isCaptain: true,
 };
 person.age = 40;
 console.log(person);
 
+// END - LESSON - 03
 
+// START - LESSON - 04
 
+let a: string;
+let b: number;
 
+a = "Hello";
+b = 4;
 
+// STRING TYPE ARRAY
+let stringArray: string[] = [];
+stringArray.push("Hello");
+
+// MIXED TYPE ARRAY
+let mixedArray: (string | number)[] = [];
+
+// OBJECT TYPE
+let c: {
+  name: string;
+  age: number;
+};
+c = {
+  name: "John",
+  age: 30,
+};
